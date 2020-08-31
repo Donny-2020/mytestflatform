da94334d8dcaf07543e1b70c6be387ed410d6ff4
six <981772991@qq.com>
2020-08-31 20:42:07 +0800

    提交web自动化应用app

--- mytestplatform/urls.py
+++ mytestplatform/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webauto import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('webauto/',views.webauto)
 ]
